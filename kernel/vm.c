b88b4055885e76c17f2f71eedab924a266ab6994
jmu201621143028 <2216164102@qq.com>
2022-05-21 23:00:17 -0700

    used to understand the kvmmap function

--- kernel/vm.c
+++ kernel/vm.c
@@ -39,9 +39,15 @@ kvmmake(void)
   // map kernel data and the physical RAM we'll make use of.
   kvmmap(kpgtbl, (uint64)etext, (uint64)etext, PHYSTOP-(uint64)etext, PTE_R | PTE_W);
 
+  uint64 tram_0 = walkaddr_test(kpgtbl, TRAMPOLINE);
+  printf("%p\n", tram_0);
+
   // map the trampoline for trap entry/exit to
   // the highest virtual address in the kernel.
   kvmmap(kpgtbl, TRAMPOLINE, (uint64)trampoline, PGSIZE, PTE_R | PTE_X);
+
+  uint64 tram_1 = walkaddr_test(kpgtbl, TRAMPOLINE);
+  printf("%p\n", tram_1);
 
   // map kernel stacks
   proc_mapstacks(kpgtbl);
@@ -115,6 +121,24 @@ walkaddr(pagetable_t pagetable, uint64 va)
   if((*pte & PTE_V) == 0)
     return 0;
   if((*pte & PTE_U) == 0)
+    return 0;
+  pa = PTE2PA(*pte);
+  return pa;
+}
+
+uint64
+walkaddr_test(pagetable_t pagetable, uint64 va)
+{
+  pte_t *pte;
+  uint64 pa;
+
+  if(va >= MAXVA)
+    return 0;
+
+  pte = walk(pagetable, va, 0);
+  if(pte == 0)
+    return 0;
+  if((*pte & PTE_V) == 0)
     return 0;
   pa = PTE2PA(*pte);
   return pa;
